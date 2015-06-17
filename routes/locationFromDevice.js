9ab64c2cf469d58a5151dfec50cf1322e0d0572f
jas92 <jas92singh92@gmail.com>
2015-06-17 18:20:19 +0530

    integrating map

--- routes/locationFromDevice.js
+++ routes/locationFromDevice.js
@@ -7,19 +7,42 @@ router.use(function log(req, res, next){
 });
 
 router.get('/', function(req, res){
-	res.render('traceMap',{
-		title:"Home page TW", 
-		content: "Hello World"
-	});
+	
 });
 
 router.post('/', function(req, res){
 	//var latitude = req.body.latitude;
 	//var longitude = req.body.longitude;
 	//var altitude = req.body.altitude;
-  res.send("Got a POST request!");
+	//res.send("Got a POST request!");
 
-  
+	var assetName = req.body.assetName;
+
+	console.log(assetName);
+
+	var allTrace = [];
+
+	db.get(assetName, function(err, body){
+		if(typeof body == "undefined"){
+			console.log("No such asset exists");
+		}
+
+		else{
+			console.log("Fetching trace");
+
+
+
+			allTrace = allTrace.concat(body.trace);
+
+			console.log(allTrace);
+
+			res.render('traceMap', {title:"Home page TW", content: allTrace});
+
+		}
+
+
+	});
+
 
 });
 
