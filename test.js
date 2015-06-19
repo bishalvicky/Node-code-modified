50fcbc414d95ab1bf54f0ff2a247a960b9905699
Bishal Sahoo <bishalvicky@gmail.com>
2015-06-19 16:16:38 +0530

    Ver 2.0

--- test.js
+++ test.js
@@ -1,18 +1,3 @@
-/*
- * Copyright 2014 IBM Corp. All Rights Reserved
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 var express = require('express'),
     bodyparser = require('body-parser'),
     ibmbluemix = require('ibmbluemix'),
@@ -24,6 +9,7 @@ var appConfig = {
     applicationRoute: "http://node-code.mybluemix.net",
     applicationSecret: "d59b875fbe53ed4340df9304747bf182d2bba0ea"
 };
+
 // create an express app
 var app = express();
 app.use(bodyparser.json());
@@ -38,60 +24,47 @@ app.use(bodyparser.urlencoded({
 
 //initialize mbaas-config module
 ibmbluemix.initialize(appConfig);
-var logger = ibmbluemix.getLogger();
+//var logger = ibmbluemix.getLogger();
+var push=ibmpush.initializeService();;
 
-app.use(function(req, res, next) {
-	console.log("Using...");
-	req.ibmpush = ibmpush.initializeService(req);
-	req.logger = logger;
-	next();
-});
 
 //initialize ibmconfig module
-var ibmconfig = ibmbluemix.getConfig();
+//var ibmconfig = ibmbluemix.getConfig();
 
 //get context root to deploy your application
 //the context root is '${appHostName}/v1/apps/${applicationId}'
-var contextRoot = ibmconfig.getContextRoot();
-appContext=express.Router();
-app.use(contextRoot, appContext);
+//var contextRoot = ibmconfig.getContextRoot();
+//appContext=express.Router();
+//app.use(contextRoot, appContext);
 
-console.log("contextRoot: " + contextRoot);
+//console.log("contextRoot: " + contextRoot);
 
 // log all requests
-app.all('*', function(req, res, next) {
-	console.log("Received request to " + req.url);
-	next();
-});
+
 
 // create resource URIs
 // endpoint: https://mobile.ng.bluemix.net/${appHostName}/v1/apps/${applicationId}/notifyOtherDevices/
-appContext.post('/notifyOtherDevices', function(req,res) {
-	var results = 'Sent notification to all registered devices successfully.';
+	//var results = 'Sent notification to all registered devices successfully.';
 
 	console.log("Trying to send push notification via JavaScript Push SDK");
 	var message = { "alert" : "The BlueList has been updated.",
 					"url": "http://www.google.com"
 	};
 
-	req.ibmpush.sendBroadcastNotification(message,null).then(function (response) {
-		console.log("Notification sent successfully to all devices.", response);
+	push.sendBroadcastNotification(message,null).then(function (response) {
+		console.log("Notification sent successfully to all devices.");
 		res.send("Sent notification to all registered devices.");
 	}, function(err) {
 		console.log("Failed to send notification to all devices.");
 		console.log(err);
 		res.send(400, {reason: "An error occurred while sending the Push notification.", error: err});
 	});
-});
+
 
 // host static files in public folder
 // endpoint:  https://mobile.ng.bluemix.net/${appHostName}/v1/apps/${applicationId}/static/
-appContext.use('/static', express.static('public'));
+//appContext.use('/static', express.static('public'));
 
-//redirect to cloudcode doc page when accessing the root context
-app.get('/', function(req, res){
-	res.sendfile('public/index.html');
-});
 
-app.listen(ibmconfig.getPort());
-console.log('Server started at port: '+ibmconfig.getPort());
+//app.listen(ibmconfig.getPort());
+//console.log('Server started at port: '+ibmconfig.getPort());
